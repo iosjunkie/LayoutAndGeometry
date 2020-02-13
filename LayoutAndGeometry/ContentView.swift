2850a195a92f330503934e9891fc674e8a255005
Jules Lee <jackconsolidated@gmail.com>
2020-02-13 21:22:04 +0800

    Coverflow effect

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        Text("Hello, World!")
+    GeometryReader { fullView in
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Rectangle()
+                            .fill(self.colors[index % 7])
+                            .frame(height: 150)
+                            .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).midX - fullView.size.width / 2) / 10), axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(width: 150)
+                }
+            }
+            .padding(.horizontal, ((fullView.size.width - 150) / 2))
+        }
+    }
+    .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -18,4 +36,14 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+extension VerticalAlignment {
+    struct MidAccountAndName: AlignmentID {
+        static func defaultValue(in d: ViewDimensions) -> CGFloat {
+            d[.top]
+        }
+    }
+
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
 }
